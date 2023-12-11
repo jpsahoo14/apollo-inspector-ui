da8497a1388c0986ffb5d0b45dde7b013f2ac438
Vyshnavi Dasari <vydasari@microsoft.com>
2023-12-12 00:24:01 +0530

    Removed typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
       node: true,
       es2021: true,
     },
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
+    extends: [
+      'eslint:recommended', 
+      // 'plugin:@typescript-eslint/recommended', 
+      // 'plugin:react/recommended'
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
       ecmaVersion: 12,
@@ -12,10 +16,11 @@ module.exports = {
     },
     plugins: ['@typescript-eslint'],
     rules: {
-      // Added a few rules for example
-      "no-console": "warn",
-      // "eqeqeq": "error",
-      // "@typescript-eslint/no-unused-vars": "warn",
-      // "semi": ["error", "always"]
+      /** Added a few rules for example
+       * "no-console": "warn",
+       *  "eqeqeq": "error",
+       * "@typescript-eslint/no-unused-vars": "warn",
+       * "semi": ["error", "always"]
+       **/
     },
   };
