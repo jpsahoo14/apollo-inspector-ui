4645678732afd05123db212135a88dfbf24c9a3e
Diksha <dikshabhateja18@gmail.com>
2024-03-12 17:42:37 +0530

    Affected queries css fixes

--- src/affected-queries/affected-queries-grid-renderer-styles.ts
+++ src/affected-queries/affected-queries-grid-renderer-styles.ts
@@ -3,5 +3,8 @@ import { makeStyles } from "@fluentui/react-components";
 export const useStyles = makeStyles({
   root: {
     minHeight: 0,
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
   },
 });
