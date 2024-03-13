9537d2a407689f7e5bb78c8b53e1770ee7f0b714
Diksha Bhateja <dikshabhateja18@gmail.com>
2024-03-13 12:43:03 +0530

    Affected queries page css fixes (#47)
    1. Improve css for affected queries page.
    2. Added scroll option for table operations.

--- src/affected-queries/affected-queries-renderer-styles.ts
+++ src/affected-queries/affected-queries-renderer-styles.ts
@@ -1,19 +1,64 @@
-import { makeStyles } from "@fluentui/react-components";
+import { makeStyles, shorthands, tokens } from "@fluentui/react-components";
 
 export const useStyles = makeStyles({
   root: {
     display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    height: "100%",
+    width: "100%",
+  },
+  leftPaneHeader: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    ...shorthands.padding(".5rem", "0.5rem"),
+    backgroundColor: tokens.colorSubtleBackgroundHover,
+    ...shorthands.borderBottom(
+      ".1rem",
+      "solid",
+      tokens.colorNeutralShadowAmbientLighter
+    ),
+    maxWidth: "100%",
     justifyContent: "space-between",
+  },
+  leftPane: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "stretch",
     minHeight: 0,
+    minWidth: 0,
+    height: "100%",
+    backgroundColor: tokens.colorNeutralBackground2,
+    ...shorthands.borderRight(
+      ".1rem",
+      "solid",
+      tokens.colorSubtleBackgroundHover
+    ),
+  },
+  infoButton: {
+    minWidth: "auto",
+    alignItems: "end",
+    ...shorthands.border("none"),
+    backgroundColor: tokens.colorSubtleBackgroundHover,
   },
   rightPane: {
     display: "flex",
     flexDirection: "column",
+    alignItems: "stretch",
     minHeight: 0,
-    marginRight: "2rem",
+    minWidth: 0,
+    height: "100%",
+    width: "100%",
   },
   rightPaneHeader: {
     display: "flex",
-    marginBottom: "0.5rem",
+    ...shorthands.padding(".5rem", "0.5rem"),
+  },
+  selectedTab: {
+    backgroundColor: tokens.colorBrandBackground2Hover,
+    "&:hover": {
+      backgroundColor: tokens.colorBrandBackground2Hover,
+    },
   },
 });
--- src/affected-queries/affected-queries-renderer.tsx
+++ src/affected-queries/affected-queries-renderer.tsx
@@ -5,10 +5,13 @@ import {
   SelectTabEvent,
   SelectTabData,
   Text,
+  Button,
+  Tooltip,
 } from "@fluentui/react-components";
 import { AffectedQueriesGridRenderers } from "./affected-queries-grid-renderer";
 import { IDueToOperation } from "apollo-inspector";
 import { useStyles } from "./affected-queries-renderer-styles";
+import { Info20Regular } from "@fluentui/react-icons";
 
 export interface IAffectedQueriesRendererProps {
   listOfItems: any[];
@@ -21,16 +24,33 @@ export const AffectedQueriesRenderer = (
   props: IAffectedQueriesRendererProps
 ) => {
   const { listOfItems, gridItems, selectedListItem, onTabSelect } = props;
+  const [isInfoTooltipVisible, setInfoTooltipVisible] = React.useState(false);
   const classes = useStyles();
   const tabItems = listOfItems.map((element: any) => {
     return (
-      <Tab key={element.name} value={element.name}>
+      <Tab key={element.name} value={element.name} className={selectedListItem === element.name ? classes.selectedTab : ''} >
         {element.value}
       </Tab>
     );
   });
   return (
     <div className={classes.root}>
+      <div className={classes.leftPane}>
+          <div className={classes.leftPaneHeader}>
+        <Text weight="bold" size={300}><b>Re-rendered Queries</b></Text>
+        <Tooltip
+              content="Affected Queries tab helps you see which watch queries were re-rendered and why. It shows insights into the queries and the operations causing those updates."
+              visible={isInfoTooltipVisible} relationship={"label"} positioning={'after'}>
+        <Button
+          title="Information"
+          tabIndex={0}
+          className={classes.infoButton}
+          onClick={() => {
+            setInfoTooltipVisible(!isInfoTooltipVisible)}}>
+          <Info20Regular />
+        </Button>
+        </Tooltip>
+        </div>
       <TabList
         selectedValue={selectedListItem}
         onTabSelect={onTabSelect}
@@ -38,13 +58,10 @@ export const AffectedQueriesRenderer = (
       >
         {tabItems}
       </TabList>
+      </div>
       <div className={classes.rightPane}>
         <div className={classes.rightPaneHeader}>
-          <Text weight="bold" size={300}>{`${selectedListItem}`}</Text>
-          <span>&nbsp;</span>
-          <Text
-            size={300}
-          >{`is re-rendered due to following operations in the table`}</Text>
+          <Text size={300}><b>{selectedListItem}</b> is re-rendered due to following operations in the table</Text>
         </div>
         <AffectedQueriesGridRenderers items={gridItems} />
       </div>
