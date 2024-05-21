7d993d84f0526f2aea968fbaffe2a8f9033485c1
Jyoti Prakash Sahoo <jpsahoo14@gmail.com>
2024-05-21 11:15:35 +0530

    Chan

--- src/__test__/test-app/src/components/page/page-container.tsx
+++ src/__test__/test-app/src/components/page/page-container.tsx
@@ -1,19 +1,52 @@
 import * as React from "react";
 import { Page } from "./page";
 import { IPage } from "../../data/resolvers/initial-data/data.interface";
-import { Subtitle2Stronger, List, Button } from "@fluentui/react-components";
+import { Button } from "@fluentui/react-components";
+import { addSectionMutation, editorQuery } from "../../data/operations";
+import { useApolloClient } from "@apollo/client";
+import { editor } from "../../data/resolvers/query/editor-resolver";
 
 interface IPageContainerProps {
   page: IPage;
 }
 export const PageContainer = (props: IPageContainerProps) => {
   const { page } = props;
+  const client = useApolloClient();
 
-  const onBtnClick = React.useCallback(() => {}, []);
+  const onBtnClick = React.useCallback(async () => {
+    try {
+      await client.mutate({
+        mutation: addSectionMutation,
+        variables: { input: { pageId: page.id } },
+        update: (cache, result) => {
+          console.log({ cache, result });
+          const cachedData: any = cache.readQuery({
+            query: editorQuery,
+            variables: { input: { id: 1 } },
+          });
+          const updatedData = {
+            ...cachedData,
+            editor: {
+              ...cachedData.editor,
+              selectedPage: {
+                ...cachedData.editor.selectedPage,
+                sections: [
+                  ...cachedData.editor.selectedPage.sections,
+                  result.data.addSection,
+                ],
+              },
+            },
+          };
+
+          cache.writeQuery({ query: editorQuery, data: updatedData });
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }, [client, page]);
   const actionButton = (
-    <Button onClick={onBtnClick}>{`${
-      page.isEditing ? "Save" : "Edit"
-    }`}</Button>
+    <Button onClick={onBtnClick}>{`${"Add Section"}`}</Button>
   );
 
   return <Page page={page} actionButton={actionButton} />;
